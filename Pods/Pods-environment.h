018df33d7141b8900312c96f30711cde82a0f430
Colin Regan <colin@redcup.la>
2014-01-11 23:21:08 -0500

    Adds Cocoapods and KASlideshow to project

---
+++ Pods/Pods-environment.h
@@ -0,0 +1,14 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// KASlideShow
+#define COCOAPODS_POD_AVAILABLE_KASlideShow
+#define COCOAPODS_VERSION_MAJOR_KASlideShow 1
+#define COCOAPODS_VERSION_MINOR_KASlideShow 1
+#define COCOAPODS_VERSION_PATCH_KASlideShow 0
+
