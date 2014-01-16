ac162da0ce7aa2d68fc6abd030bac0c5ba504f8d
Colin Regan <colin@redcup.la>
2014-01-16 08:27:39 -0500

    Creates RCPhotoAlbum class

---
+++ Infinite Slideshow/RCPhotoAlbum.h
@@ -0,0 +1,13 @@
+//
+//  RCPhotoAlbum.h
+//  Infinite Slideshow
+//
+//  Created by Colin Regan on 1/16/14.
+//  Copyright (c) 2014 Red Cup. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RCPhotoAlbum : NSObject
+
+@end
---
+++ Infinite Slideshow/RCPhotoAlbum.m
@@ -0,0 +1,13 @@
+//
+//  RCPhotoAlbum.m
+//  Infinite Slideshow
+//
+//  Created by Colin Regan on 1/16/14.
+//  Copyright (c) 2014 Red Cup. All rights reserved.
+//
+
+#import "RCPhotoAlbum.h"
+
+@implementation RCPhotoAlbum
+
+@end
