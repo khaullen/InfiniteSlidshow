fc0efb1a4833c66ba6affb19c62ceefed398f40b
Colin Regan <colin@redcup.la>
2014-01-16 08:36:57 -0500

    Adds test class for RCPhotoAlbum

---
+++ Infinite SlideshowTests/RCPhotoAlbum_Tests.m
@@ -0,0 +1,34 @@
+//
+//  RCPhotoAlbum_Tests.m
+//  Infinite Slideshow
+//
+//  Created by Colin Regan on 1/16/14.
+//  Copyright (c) 2014 Red Cup. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface RCPhotoAlbum_Tests : XCTestCase
+
+@end
+
+@implementation RCPhotoAlbum_Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here; it will be run once, before the first test case.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here; it will be run once, after the last test case.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
