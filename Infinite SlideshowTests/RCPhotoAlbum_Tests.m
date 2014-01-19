db5468afa4bfeb6ebd13c5397dd0a93a86f7e7c6
Colin Regan <colin@redcup.la>
2014-01-18 19:00:04 -0500

    Adds proof of concept test for dispatch_semaphore

--- Infinite SlideshowTests/RCPhotoAlbum_Tests.m
+++ Infinite SlideshowTests/RCPhotoAlbum_Tests.m
@@ -36,4 +36,36 @@
     XCTAssertEqualObjects(album.groupURL, groupURL);
 }
 
+- (void)testSemaphore
+{
+    dispatch_semaphore_t sema = dispatch_semaphore_create(0);
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0), ^{
+        [NSThread sleepForTimeInterval:3.0f];
+        dispatch_semaphore_signal(sema);
+    });
+    dispatch_async(dispatch_queue_create("1", NULL), ^{
+        [NSThread sleepForTimeInterval:1.0f];
+        NSLog(@"1");
+    });
+    dispatch_async(dispatch_queue_create("2", NULL), ^{
+        [NSThread sleepForTimeInterval:2.0f];
+        NSLog(@"2");
+    });
+    dispatch_async(dispatch_queue_create("3", NULL), ^{
+        [NSThread sleepForTimeInterval:3.0f];
+        NSLog(@"3");
+    });
+    NSLog(@"Waiting...");
+    dispatch_semaphore_wait(sema, DISPATCH_TIME_FOREVER);
+    NSLog(@"Finished.");
+    XCTAssertTrue(YES);
+}
+
+// Idea for testing asynchronous methods:
+// Dependency injection -- inject mock ALAssetsLibrary object into RCPhotoAlbum
+// Subclass ALAssetsLibrary and override all its public methods to call either success or failure block immediately with mock data
+// Subclasses provide public semaphore properties for each method indicating when they've been called
+// Overridden methods call the success (or failure) block passed by class under test using mock data, then signal semaphore
+// Test methods wait on the corresponding semaphore, then check the side effects on class under test
+
 @end
