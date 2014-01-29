3172af3fcc57554830f42d762f4253ae7adbee0d
Colin Regan <colin@redcup.la>
2014-01-28 23:41:47 -0500

    Moves tests to correct class

--- Infinite SlideshowTests/Infinite_SlideshowTests.m
+++ Infinite SlideshowTests/Infinite_SlideshowTests.m
@@ -8,6 +8,49 @@
 
 #import <XCTest/XCTest.h>
 #import "RCAppDelegate.h"
+#import <AssetsLibrary/AssetsLibrary.h>
+
+@interface TCAssetsLibrary : ALAssetsLibrary
+
+@property BOOL success;
+@property (nonatomic, strong) dispatch_semaphore_t semaphore;
+
+- (instancetype)initWithSuccess:(BOOL)success;
+
+@end
+
+@implementation TCAssetsLibrary
+
+- (instancetype)initWithSuccess:(BOOL)success
+{
+    self = [super init];
+    if (self) {
+        self.success = success;
+        self.semaphore = dispatch_semaphore_create(0);
+    }
+    return self;
+}
+
+- (void)enumerateGroupsWithTypes:(ALAssetsGroupType)types usingBlock:(ALAssetsLibraryGroupsEnumerationResultsBlock)enumerationBlock failureBlock:(ALAssetsLibraryAccessFailureBlock)failureBlock
+{
+    NSError *error;
+    switch (types) {
+        case ALAssetsGroupPhotoStream:
+            if (self.success) {
+                //                enumerationBlock();
+            } else {
+                error = [NSError errorWithDomain:ALAssetsLibraryErrorDomain code:-3311 userInfo:@{NSLocalizedDescriptionKey: @"User denied access", NSUnderlyingErrorKey: [NSError errorWithDomain:ALAssetsLibraryErrorDomain code:-3311 userInfo:@{NSLocalizedDescriptionKey: @"The operation couldn’t be completed"}], NSLocalizedFailureReasonErrorKey: @"The user has denied the application access to their media"}];
+                failureBlock(error);
+                dispatch_semaphore_signal(self.semaphore);
+            }
+            break;
+            
+        default:
+            break;
+    }
+}
+
+@end
 
 @interface Infinite_SlideshowTests : XCTestCase
 
@@ -26,6 +69,46 @@
     // Put teardown code here. This method is called after the invocation of each test method in the class.
     [super tearDown];
 }
+
+- (void)testUserDeniesAccess
+{
+    TCAssetsLibrary *library = [[TCAssetsLibrary alloc] initWithSuccess:NO];
+    
+}
+
+- (void)testSemaphore
+{
+    dispatch_semaphore_t sema = dispatch_semaphore_create(0);
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0), ^{
+        [NSThread sleepForTimeInterval:3.0f];
+        dispatch_semaphore_signal(sema);
+    });
+    dispatch_async(dispatch_queue_create("1", NULL), ^{
+        [NSThread sleepForTimeInterval:1.0f];
+        NSLog(@"1");
+    });
+    dispatch_async(dispatch_queue_create("2", NULL), ^{
+        [NSThread sleepForTimeInterval:2.0f];
+        NSLog(@"2");
+    });
+    dispatch_async(dispatch_queue_create("3", NULL), ^{
+        [NSThread sleepForTimeInterval:3.0f];
+        NSLog(@"3");
+    });
+    NSLog(@"Waiting...");
+    dispatch_semaphore_wait(sema, DISPATCH_TIME_FOREVER);
+    NSLog(@"Finished.");
+    XCTAssertTrue(YES);
+}
+
+// Idea for testing asynchronous methods:
+// Dependency injection -- inject mock ALAssetsLibrary object into RCPhotoAlbum
+// Subclass ALAssetsLibrary and override all its public methods to call either success or failure block immediately with mock data
+// Subclasses provide public semaphore properties for each method indicating when they've been called
+// Overridden methods call the success (or failure) block passed by class under test using mock data, then signal semaphore
+// Test methods wait on the corresponding semaphore, then check the side effects on class under test
+
+
 
 //- (void)testSlideshowStartsOnAppLaunch
 //{
--- Infinite SlideshowTests/RCPhotoAlbum_Tests.m
+++ Infinite SlideshowTests/RCPhotoAlbum_Tests.m
@@ -10,14 +10,6 @@
 #import "RCPhotoAlbum.h"
 #import <AssetsLibrary/AssetsLibrary.h>
 
-@interface TCAssetsLibrary : ALAssetsLibrary
-
-@end
-
-@implementation TCAssetsLibrary
-
-@end
-
 @interface RCPhotoAlbum_Tests : XCTestCase
 
 @property (nonatomic, strong) RCPhotoAlbum *album;
@@ -38,50 +30,11 @@
     [super tearDown];
 }
 
-- (void)testInitWithGroupURL
-{
-    NSURL *groupURL = [NSURL URLWithString:@"test"];
-    RCPhotoAlbum *album = [[RCPhotoAlbum alloc] initWithGroupURL:groupURL];
-    XCTAssertEqualObjects(album.groupURL, groupURL);
-}
-
 - (void)testInitWithSource
 {
-    TCAssetsLibrary *library = [[TCAssetsLibrary alloc] init];
-    RCPhotoAlbum *album = [[RCPhotoAlbum alloc] initWithSource:library];
-    XCTAssertEqualObjects(album.source, library);
+    ALAssetsGroup *group = [[ALAssetsGroup alloc] init];
+    RCPhotoAlbum *album = [[RCPhotoAlbum alloc] initWithSource:group];
+    XCTAssertEqualObjects(album.source, group);
 }
-
-- (void)testSemaphore
-{
-    dispatch_semaphore_t sema = dispatch_semaphore_create(0);
-    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0), ^{
-        [NSThread sleepForTimeInterval:3.0f];
-        dispatch_semaphore_signal(sema);
-    });
-    dispatch_async(dispatch_queue_create("1", NULL), ^{
-        [NSThread sleepForTimeInterval:1.0f];
-        NSLog(@"1");
-    });
-    dispatch_async(dispatch_queue_create("2", NULL), ^{
-        [NSThread sleepForTimeInterval:2.0f];
-        NSLog(@"2");
-    });
-    dispatch_async(dispatch_queue_create("3", NULL), ^{
-        [NSThread sleepForTimeInterval:3.0f];
-        NSLog(@"3");
-    });
-    NSLog(@"Waiting...");
-    dispatch_semaphore_wait(sema, DISPATCH_TIME_FOREVER);
-    NSLog(@"Finished.");
-    XCTAssertTrue(YES);
-}
-
-// Idea for testing asynchronous methods:
-// Dependency injection -- inject mock ALAssetsLibrary object into RCPhotoAlbum
-// Subclass ALAssetsLibrary and override all its public methods to call either success or failure block immediately with mock data
-// Subclasses provide public semaphore properties for each method indicating when they've been called
-// Overridden methods call the success (or failure) block passed by class under test using mock data, then signal semaphore
-// Test methods wait on the corresponding semaphore, then check the side effects on class under test
 
 @end
