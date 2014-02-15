af69ece3c6f2652d538a327f10c6e5a1ed861ce0
Colin Regan <colin@redcup.la>
2014-02-15 01:41:43 -0500

    Adds notification listening for asset library changes

--- InfiniteSlideshow/RCPhotoAlbum.m
+++ InfiniteSlideshow/RCPhotoAlbum.m
@@ -16,16 +16,25 @@ static NSString *const kRCUserDeniedAccessMessage = @"This app requires photo li
 
 @interface RCPhotoAlbum ()
 
+@property (strong, nonatomic) NSMutableArray *loadedPhotos;
+
+- (void)assetsChanged:(NSNotification *)notification;
+- (void)loadImagesFromSource:(ALAssetsGroup *)source;
+
 @end
 
 @implementation RCPhotoAlbum
+
+#pragma mark - Initialization
 
 - (instancetype)initWithLibrary:(ALAssetsLibrary *)library groupName:(NSString *)groupName
 {
     self = [super init];
     if (self) {
+        self.loadedPhotos = [NSMutableArray new];
         if (library) {
             self.library = library;
+            [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(assetsChanged:) name:ALAssetsLibraryChangedNotification object:library];
             [library enumerateGroupsWithTypes:ALAssetsGroupPhotoStream usingBlock:^(ALAssetsGroup *group, BOOL *stop) {
                 NSString *name = [group valueForProperty:ALAssetsGroupPropertyName];
                 if ([groupName isEqualToString:name]) {
@@ -49,24 +58,38 @@ static NSString *const kRCUserDeniedAccessMessage = @"This app requires photo li
     return [self initWithLibrary:nil groupName:nil];
 }
 
+#pragma mark - Image loading
+
 - (void)setSource:(ALAssetsGroup *)source
 {
     if (_source != source) {
         _source = source;
-        NSMutableArray *photos = [NSMutableArray new];
-        [source enumerateAssetsUsingBlock:^(ALAsset *result, NSUInteger index, BOOL *stop) {
-            if (result) {
-                if ([[result valueForProperty:ALAssetPropertyType] isEqualToString:ALAssetTypePhoto]) {
+        [self loadImagesFromSource:source];
+    }
+}
+
+- (void)loadImagesFromSource:(ALAssetsGroup *)source
+{
+    NSMutableArray *photos = [NSMutableArray new];
+    [source enumerateAssetsUsingBlock:^(ALAsset *result, NSUInteger index, BOOL *stop) {
+        if (result) {
+            if ([[result valueForProperty:ALAssetPropertyType] isEqualToString:ALAssetTypePhoto]) {
+                if (![self.loadedPhotos containsObject:result]) {
                     ALAssetRepresentation *rep = [result defaultRepresentation];
                     CGImageRef imageRef = rep ? [rep fullScreenImage] : [result aspectRatioThumbnail];
                     UIImage *image = [UIImage imageWithCGImage:imageRef];
-                    if (image) [photos addObject:image];
+                    if (image) {
+                        [photos addObject:image];
+                        [self.loadedPhotos addObject:result];
+                    }
                 }
             }
-        }];
-        [self.delegate photoAlbum:self didLoadNewPhotos:photos];
-    }
+        }
+    }];
+    [self.delegate photoAlbum:self didLoadNewPhotos:photos];
 }
+
+#pragma mark - Properties
 
 - (NSString *)name
 {
@@ -86,6 +109,21 @@ static NSString *const kRCUserDeniedAccessMessage = @"This app requires photo li
 - (NSURL *)url
 {
     return [self.source valueForProperty:ALAssetsGroupPropertyURL];
+}
+
+#pragma mark - Notifications
+
+- (void)assetsChanged:(NSNotification *)notification
+{
+    NSSet *updatedGroups = notification.userInfo[ALAssetLibraryUpdatedAssetGroupsKey];
+    if ([updatedGroups containsObject:self.url]) {
+        [self loadImagesFromSource:self.source];
+    }
+}
+
+- (void)dealloc
+{
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
 }
 
 @end
