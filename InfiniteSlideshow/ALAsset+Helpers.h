a2efbd7c8ded668eadbeb61380a67070e61bd970
Colin Regan <colin@redcup.la>
2014-02-15 17:31:21 -0500

    Adds ALAsset helper category

---
+++ InfiniteSlideshow/ALAsset+Helpers.h
@@ -0,0 +1,13 @@
+//
+//  ALAsset+Helpers.h
+//  InfiniteSlideshow
+//
+//  Created by Colin Regan on 2/15/14.
+//  Copyright (c) 2014 Red Cup. All rights reserved.
+//
+
+#import <AssetsLibrary/AssetsLibrary.h>
+
+@interface ALAsset (Helpers)
+
+@end
---
+++ InfiniteSlideshow/ALAsset+Helpers.m
@@ -0,0 +1,13 @@
+//
+//  ALAsset+Helpers.m
+//  InfiniteSlideshow
+//
+//  Created by Colin Regan on 2/15/14.
+//  Copyright (c) 2014 Red Cup. All rights reserved.
+//
+
+#import "ALAsset+Helpers.h"
+
+@implementation ALAsset (Helpers)
+
+@end
