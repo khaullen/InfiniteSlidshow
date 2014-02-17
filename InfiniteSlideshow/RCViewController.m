f858a3e5bb90b2cbc80d3f3ad14f9136335a9cea
Colin Regan <colin@redcup.la>
2014-02-16 23:39:32 -0500

    Changes hardcoded album

--- InfiniteSlideshow/RCViewController.m
+++ InfiniteSlideshow/RCViewController.m
@@ -10,7 +10,7 @@
 #import <AssetsLibrary/AssetsLibrary.h>
 #import "ALAsset+Helpers.h"
 
-static NSString *const kRCHardcodedGroupName = @"Patterns";
+static NSString *const kRCHardcodedGroupName = @"Grandpa";
 
 @interface RCViewController ()
 
