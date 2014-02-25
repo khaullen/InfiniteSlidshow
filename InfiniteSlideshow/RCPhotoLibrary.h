2697e766abddbbc44e7535618377c438fcde94c0
Colin Regan <colin@redcup.la>
2014-02-24 23:16:22 -0500

    Adds RCPhotoLibrary class

---
+++ InfiniteSlideshow/RCPhotoLibrary.h
@@ -0,0 +1,13 @@
+//
+//  RCPhotoLibrary.h
+//  InfiniteSlideshow
+//
+//  Created by Colin Regan on 2/24/14.
+//  Copyright (c) 2014 Red Cup. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RCPhotoLibrary : NSObject
+
+@end
---
+++ InfiniteSlideshow/RCPhotoLibrary.m
@@ -0,0 +1,13 @@
+//
+//  RCPhotoLibrary.m
+//  InfiniteSlideshow
+//
+//  Created by Colin Regan on 2/24/14.
+//  Copyright (c) 2014 Red Cup. All rights reserved.
+//
+
+#import "RCPhotoLibrary.h"
+
+@implementation RCPhotoLibrary
+
+@end
