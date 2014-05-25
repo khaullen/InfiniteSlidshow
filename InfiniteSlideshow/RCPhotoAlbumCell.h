27dd99e8d2a3886226c8cbf4ada7faee3f1d3892
Colin Regan <colin@redcup.la>
2014-05-24 21:20:28 -0400

    Creates RCPhotoAlbumCell class

---
+++ InfiniteSlideshow/RCPhotoAlbumCell.h
@@ -0,0 +1,13 @@
+//
+//  RCPhotoAlbumCell.h
+//  InfiniteSlideshow
+//
+//  Created by Colin Regan on 5/24/14.
+//  Copyright (c) 2014 Red Cup. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RCPhotoAlbumCell : UICollectionViewCell
+
+@end
---
+++ InfiniteSlideshow/RCPhotoAlbumCell.m
@@ -0,0 +1,31 @@
+//
+//  RCPhotoAlbumCell.m
+//  InfiniteSlideshow
+//
+//  Created by Colin Regan on 5/24/14.
+//  Copyright (c) 2014 Red Cup. All rights reserved.
+//
+
+#import "RCPhotoAlbumCell.h"
+
+@implementation RCPhotoAlbumCell
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
